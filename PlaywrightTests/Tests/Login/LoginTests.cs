f542dc45e7d4aabe33a35f3fb32ab93f6f0873ef
hectorsandovalcordon-qa <hectorsandovalcordon@gmail.com>
2025-06-19 14:52:22 +0200

    feat: allure complete

--- PlaywrightTests/Tests/Login/LoginTests.cs
+++ PlaywrightTests/Tests/Login/LoginTests.cs
@@ -1,6 +1,11 @@
+using Allure.Xunit.Attributes;
+
 [Collection("Playwright collection")]
+[AllureSuite("Login Tests")]
 public class LoginTests(PlaywrightFixture fixture) : BaseTest(fixture)
 {
+    [AllureFeature("Login")]
+    [AllureStory("Successful login")]
     [Fact]
     public async Task Should_Login_Successfully()
     {
