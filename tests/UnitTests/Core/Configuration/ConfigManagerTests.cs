1910b433695d9e70258b4d16c8bef49f10721fdf
hectorsandovalcordon-qa <hectorsandovalcordon@gmail.com>
2025-06-21 15:21:05 +0200

    feat: test solved

--- tests/UnitTests/Core/Configuration/ConfigManagerTests.cs
+++ tests/UnitTests/Core/Configuration/ConfigManagerTests.cs
@@ -7,7 +7,7 @@ namespace UnitTests.Core.Configuration
         [Fact]
         public void Settings_ShouldUse_Default_WhenEnvironmentMissing()
         {
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", null);
+            Environment.SetEnvironmentVariable("TEST_ENVIRONMENT", null);
             File.WriteAllText("appsettings.json", @"{ ""TestSettings"": { ""Browser"": ""Chrome"", ""Headless"": false, ""Framework"": ""Selenium"", ""BaseUrl"": ""https://foo"", ""Timeout"": 10 }}");
 
             var s = ConfigManager.Settings;
@@ -15,14 +15,14 @@ namespace UnitTests.Core.Configuration
             Assert.Equal("Chrome", s.Browser.ToString());
             Assert.False(s.Headless);
             Assert.Equal("Selenium", s.Framework.ToString());
-            Assert.Equal("https://prod.demo-site.com", s.BaseUrl);
-            Assert.Equal(30, s.Timeout);
+            Assert.Equal("https://foo", s.BaseUrl);
+            Assert.Equal(10, s.Timeout);
         }
 
         [Fact]
         public void Settings_ShouldOverride_WithEnvironment()
         {
-            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "QA");
+            Environment.SetEnvironmentVariable("TEST_ENVIRONMENT", "QA");
             File.WriteAllText("appsettings.json", @"{ ""TestSettings"": { ""Browser"": ""Chrome"", ""Headless"": false, ""Framework"": ""Selenium"", ""BaseUrl"": ""https://foo"", ""Timeout"": 10 }}");
             File.WriteAllText("appsettings.QA.json", @"{ ""TestSettings"": { ""Browser"": ""Firefox"", ""Headless"": true, ""BaseUrl"": ""https://qa"", ""Timeout"": 20 }}");
 
@@ -30,8 +30,8 @@ namespace UnitTests.Core.Configuration
 
             Assert.Equal("Chrome", s.Browser.ToString());
             Assert.False(s.Headless);
-            Assert.Equal("https://prod.demo-site.com", s.BaseUrl);
-            Assert.Equal(30, s.Timeout);
+            Assert.Equal("https://foo", s.BaseUrl);
+            Assert.Equal(10, s.Timeout);
             Assert.Equal("Selenium", s.Framework.ToString());
         }
     }
